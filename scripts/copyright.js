6d4425456d5fbd8b557aea82e3718a3da64c4b01
nancy <dearrrnancy@gmail.com>
2023-06-18 16:31:26 -0400

    updated copyright.js to include project's start year

--- scripts/copyright.js
+++ scripts/copyright.js
@@ -1,4 +1,4 @@
 const footerEl = document.querySelector(".footer");
 const currentYear = new Date().getFullYear();
 
-footerEl.textContent = `\u00A9 ${currentYear} /// (^・(oo)・^))))~`
+footerEl.textContent = `\u00A9 2021 - ${currentYear} /// (^・(oo)・^))))~`
